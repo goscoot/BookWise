134886731d2c0ae901fbe94d8019037e1cadda42
Kamilosb <Kamilosb.kk@gmail.com>
2023-06-05 22:30:14 +0200

    deleting unnecesary code

--- server/routers/books.js
+++ server/routers/books.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('../schemas/books')
-const circularJSON = require('circular-json')
 
 router.get('/', async (req, res) => {
     try {
         await Book.find().then((all) => {
-            const str = circularJSON.stringify(all)
-            JSON.parse(str)
+            const str = JSON.stringify(all)
             res.send(str)
         })
     } catch(err){
