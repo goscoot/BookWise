b76eccabe9cc8a9c947cfde25e27527939dbe560
masterHAWK99 <maciej.pochron99@gmail.com>
2023-06-09 11:49:48 +0200

    Remove duplicated key prop

--- client/src/components/navigation.tsx
+++ client/src/components/navigation.tsx
@@ -25,7 +25,6 @@ export function Navigation({
                 isActive ? "text-gray-light font-semibold" : "text-gray"
               }`}
               href={link.href}
-              key={link.name}
             >
               {link.name}
             </Link>
